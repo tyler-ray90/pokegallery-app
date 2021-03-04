f4e0ffc227b4ca80a6d6a28eaa7345dc7509f5da
Tyler Ray <tyler.p.ray@icloud.com>
2021-03-04 13:28:41 -0500

    added search bar

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// IPO Patter for program design - Input -> Process -> Output 
+
+
+/*-------constants------*/
+
+
+
+// const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+
+
+
+
+
+// /*------appps state (variables)*/
+
+
+// function getData() {
+//     $.ajax(BASE_URL)
+//     .then(funciton(data) {
+//         console.log('data: ',  data);
+//     }, function(error) {
+//         console.log('error: ', error);
+//     });
+
+// }
