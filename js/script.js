e425500e2467da092ef921cd35fcc0dbfd2ab384
Tyler Ray <tyler.p.ray@icloud.com>
2021-03-05 17:16:14 -0500

    Added search to JavaScript

--- js/script.js
+++ js/script.js
@@ -1,37 +1,32 @@
 // // IPO Patter for program design - Input -> Process -> Output 
 
 
-// /*-------constants------*/
+// // /*-------constants------*/
 
-// const $name = $('#name');
-// const $game_index = $('#game_index');
+const $name = $('#name');
+const $game_index = $('#game_index');
+const $sprites = $('#sprites');
+const $input = $('input[type="text"]');
+
+// // // Event listeners
+
+$('form').on('submit', handleSubmit);
 
 
-// // const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+// // // Functions
 
-// // Event listeneres
+function handleSubmit(evt) {
+    evt.preventDefault();
+    const $term = $input.val();
+    $.ajax(`https://pokeapi.co/api/v2/pokemon/${$term}`).then(function (data) {
+        console.log('Pokemon', data);
+        $name.text(data.name);
+        $game_index.text(data.id);
+        $sprites.html(`<img src="${data.sprites.front_default}" id="sprite"></img>`);
 
-// $('form').on('submit', handleSubmit);
+    // // }, function(error) {
+    // //     console.log('error: ', error);
+    });
 
-
-// // Functions
-
-// function handleSubmit(evt) {
-//     const term = $input.val();
-//     $.ajax(`https://pokeapi.co/api/v2/pokemon/?q=({term}`).then(funciton(data) {
-//         console.log('Pokemon', data);
-//         $name.text(data.name);
-//         $game_index.text(data.game_index);
-
-//     }, function(error) {
-//         console.log('error: ', error);
-//     });
-
-// }
-
-const poke_container = document.getElementById('poke_container');
-const pokemons_number = 150;
-
-const getPokemon = async id => {
-    const url = https://pokeapi.co/api/v2/pokemon/
 }
+
