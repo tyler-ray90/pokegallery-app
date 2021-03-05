29e6cd3d4f8a8a461a16c47103b7424afd1aaf17
Tyler Ray <tyler.p.ray@icloud.com>
2021-03-04 21:39:23 -0500

    restarting javascript

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
-// IPO Patter for program design - Input -> Process -> Output 
+// // IPO Patter for program design - Input -> Process -> Output 
 
 
-/*-------constants------*/
+// /*-------constants------*/
+
+// const $name = $('#name');
+// const $game_index = $('#game_index');
 
 
+// // const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
 
-// const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+// // Event listeneres
 
-
-
-
-
-// /*------appps state (variables)*/
+// $('form').on('submit', handleSubmit);
 
 
-// function getData() {
-//     $.ajax(BASE_URL)
-//     .then(funciton(data) {
-//         console.log('data: ',  data);
+// // Functions
+
+// function handleSubmit(evt) {
+//     const term = $input.val();
+//     $.ajax(`https://pokeapi.co/api/v2/pokemon/?q=({term}`).then(funciton(data) {
+//         console.log('Pokemon', data);
+//         $name.text(data.name);
+//         $game_index.text(data.game_index);
+
 //     }, function(error) {
 //         console.log('error: ', error);
 //     });
 
 // }
+
+const poke_container = document.getElementById('poke_container');
+const pokemons_number = 150;
+
+const getPokemon = async id => {
+    const url = https://pokeapi.co/api/v2/pokemon/
+}
