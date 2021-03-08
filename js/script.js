c09e2ce10b2f8736b78be8307e7abca86ce027e5
Tyler Ray <tyler.p.ray@icloud.com>
2021-03-07 21:19:10 -0500

    added second DIV bar with shiny pokemon

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@
 
 // // /*-------constants------*/
 
-const $name = $('#name');
-const $game_index = $('#game_index');
-const $sprites = $('#sprites');
+const $name = $('.name');
+const $game_index = $('.game_index');
+const $sprites = $('.sprites');
+const $type = $('.type');
+const $shiny = $('.shiny');
 const $input = $('input[type="text"]');
-
 // // // Event listeners
 
 $('form').on('submit', handleSubmit);
@@ -21,11 +22,10 @@ function handleSubmit(evt) {
     $.ajax(`https://pokeapi.co/api/v2/pokemon/${$term}`).then(function (data) {
         console.log('Pokemon', data);
         $name.text(data.name);
-        $game_index.text('# ' + (data.id));
+        $game_index.text('No. ' + (data.id));
         $sprites.html(`<img src="${data.sprites.front_default}" id="sprite"></img>`);
-
-    // // }, function(error) {
-    // //     console.log('error: ', error);
+        $type.text('Type: ' + (data.types[0].type.name));
+        $shiny.html(`<img src="${data.sprites.front_shiny}" id="sprite"></img>`);
     });
 
 }
